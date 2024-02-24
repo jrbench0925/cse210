011b7858f8ef34563a345de7efb73a16056a8a22
jrbench0925 <ben07010@byui.edu>
2024-02-24 10:50:47 -0700

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string keepPlaying = "yes";
+
+        while (keepPlaying == "yes")
+        {
+            int guess = -1;
+            Random firstNumber = new Random();
+            int number = firstNumber.Next (1, 1000);
+            int guessCount = 0;
+
+            while (guess != number )
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (number > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (number < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else 
+                {
+                    Console.WriteLine("You guessed it!");
+                    if (guessCount > 1)
+                    {
+                        Console.WriteLine($"It took you {guessCount} guesses");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"It only took you {guessCount} guess!");
+                    }
+                }
+            }
+            Console.Write("Would you like to play again? ");
+            keepPlaying = Console.ReadLine();
+            keepPlaying = keepPlaying.ToLower();
+            Console.WriteLine ($"Keep playing is {keepPlaying}");
+        }
     }
 }
